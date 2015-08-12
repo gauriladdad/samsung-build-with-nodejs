7db8b017c7471d0d608656ede803b577e82d608b
Gauri Gattani <Gauri.Gattani@massive-gauri.massive.com.au>
2015-08-12 13:14:14 +0100

    take user input from command line

--- start.js
+++ start.js
@@ -1,15 +1,31 @@
 var processXML = require("./updateWidget.js");
 var zip = require("./zip.js");
-
+ 
 var sourceDir ="C:/dev/dtag/bin/debug/samsung";
 var destinationPath="C:/dev/dtag/bin/debug";
 var zipName = "samsung";
 var IPaddress = "http://10.1.1.189";
 
+var prompt = require('prompt');
+ 
+	// Start the prompt 
+	prompt.start();
+  
+	// Get two properties from the user: username and email 
+	prompt.get(['sourceDir', 'destinationPath', 'zipName', 'IPaddress'], function (err, result) {
+    
+	sourceDir = result.sourceDir;
+	destinationPath = result.destinationPath;
+	zipName = result.zipName;
+	IPaddress = result.IPaddress;
+});
 
-zip.createZIP(sourceDir, destinationPath, zipName);
 
-processXML.processWidgetXML(destinationPath, "widgetlist.xml", zipName, IPaddress);	
+
+
+//zip.createZIP(sourceDir, destinationPath, zipName);
+
+//processXML.processWidgetXML(destinationPath, "widgetlist.xml", zipName, IPaddress);	
 
 	
 
